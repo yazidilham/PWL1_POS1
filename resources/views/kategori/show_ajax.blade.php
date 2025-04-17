a538641c0a39a923823c3b759ead9d3471446b42
= <=>
2025-04-17 10:34:53 +0700

    kategori

---
+++ resources/views/kategori/show_ajax.blade.php
@@ -0,0 +1,52 @@
+@empty($kategori)
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Kesalahan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="alert alert-danger">
+                    <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
+                    Data kategori tidak ditemukan.
+                </div>
+                <a href="{{ url('/kategori') }}" class="btn btn-warning">Kembali</a>
+            </div>
+        </div>
+    </div>
+@else
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Detail Data Kategori</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                <div class="alert alert-info">
+                    <h5><i class="icon fas fa-info-circle"></i> Informasi</h5>
+                    Berikut adalah detail data kategori yang dipilih.
+                </div>
+
+                <table class="table table-sm table-bordered table-striped">
+                    <tr>
+                        <th class="text-right col-4">Kode Kategori:</th>
+                        <td class="col-8">{{ $kategori->kategori_kode }}</td>
+                    </tr>
+                    <tr>
+                        <th class="text-right col-4">Nama Kategori:</th>
+                        <td class="col-8">{{ $kategori->kategori_nama }}</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+            </div>
+        </div>
+    </div>
+@endempty
