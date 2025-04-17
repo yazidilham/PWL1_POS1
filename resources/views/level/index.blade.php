0c120764286d3b548ee5ea29228a55057d2aee8b
= <=>
2025-04-17 09:10:19 +0700

    level

---
+++ resources/views/level/index.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.template')
+
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <a class="btn btn-sm btn-primary mt-1" href="{{ url('level/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('level/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
+                    Ajax</button>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_level">
+                <thead>
+                    <tr>
+                        <th>ID Level</th>
+                        <th>Kode Level</th>
+                        <th>Nama Level</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
+            data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+@endsection
+
+    @push('css')
+    @endpush
+
+    @push('js')
+        <script>
+            function modalAction(url = '') {
+                $('#myModal').load(url, function () {
+                    $('#myModal').modal('show');
+                });
+            }
+            var dataLevel
+            $(document).ready(function () {
+                dataLevel = $('#table_level').DataTable({
+                    // serverSide: true, jika ingin menggunakan server side processing 
+                    serverSide: true,
+                    ajax: {
+                        "url": "{{ url('level/list') }}",
+                        "dataType": "json",
+                        "type": "POST",
+                    },
+                    columns: [
+                        {  // nomor urut dari laravel datatable addIndexColumn() 
+                            data: "DT_RowIndex",
+                            className: "text-center",
+                            orderable: false,
+                            searchable: false
+                        }, {
+                            data: "level_kode",
+                            className: "",
+                            // orderable: true, jika ingin kolom ini bisa diurutkan  
+                            orderable: true,
+                            // searchable: true, jika ingin kolom ini bisa dicari 
+                            searchable: true
+                        }, {
+                            data: "level_nama",
+                            className: "",
+                            orderable: true,
+                            searchable: true
+                        }, {
+                            data: "aksi",
+                            className: "",
+                            orderable: false,
+                            searchable: false
+                        }
+                    ]
+                });
+                $('#level_id').on('change', function () {
+                    dataLevel.ajax.reload();
+                });
+            }); 
+        </script>
+    @endpush
